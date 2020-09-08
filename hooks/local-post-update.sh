70afd284a069b2ba595a7cb4ce003901c9e24a6e
nickgkg <nickgkg@gmail.com>
2020-09-08 17:51:27 -0400

    automatic updates after you've run update

---
+++ hooks/local-post-update.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+./github/linters/setup_local-post-update.sh
---
+++ hooks/setup_local-post-update.sh
@@ -0,0 +1,20 @@
+# Move to the root of the project
+original_pwd=$(pwd);
+cd $(dirname $0);
+cd ../..;
+
+if ! test -h .git/hooks/pre-commit; then
+    ./github/linters/setup_pre-commit.sh
+fi;
+
+mkdir -p .github/workflows
+cp .github/linters/linter-workflow.yml .github/workflows/linter-workflow.yml
+
+# Setup the pre commit hook
+if ! test -h .git/modules/.github/linters/hooks/post-update; then
+    ln -s ../../.github/hooks/local-post-update.sh .git/modules/.github/linters/hooks/post-update;
+    chmod 777 .git/modules/.github/linters/hooks/post-update;
+fi;
+
+# Restore working directory
+cd $original_pwd;
