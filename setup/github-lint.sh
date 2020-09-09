f8f60201671959aedf6f9ffde09a47b62d3e4a58
nickgkg <nickgkg@gmail.com>
2020-09-09 12:39:16 -0400

    post merge hook instead

--- setup/github-lint.sh
+++ setup/github-lint.sh
@@ -16,9 +16,9 @@ cp .github/linters/linter-workflow.yml .github/workflows/linter-workflow.yml
 git add .github
 
 # Setup the pre commit hook
-if ! test -h .git/modules/.github/linters/hooks/post-update; then
-    ln -s ../../../../../.github/linters/hooks/local-post-update.sh .git/modules/.github/linters/hooks/post-update;
-    chmod 777 .git/modules/.github/linters/hooks/post-update;
+if ! test -h .git/modules/.github/linters/hooks/post-merge; then
+    ln -s ../../../../../.github/linters/hooks/local-post-merge.sh .git/modules/.github/linters/hooks/post-merge;
+    chmod 777 .git/modules/.github/linters/hooks/post-merge;
 fi;
 
 # Restore working directory
