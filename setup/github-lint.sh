1f6bccd2197f3d9fa5e8d08398a7fdf31448cc86
nickgkg <nickgkg@gmail.com>
2020-09-10 17:44:11 -0400

    auto update on parent repo pull

--- setup/github-lint.sh
+++ setup/github-lint.sh
@@ -12,10 +12,14 @@ cp .github/linters/linter-workflow.yml .github/workflows/linter-workflow.yml
 echo $(pwd);
 git add ./.github
 
-# Setup the pre commit hook
+# Setup the post merge hooks
 rm .git/modules/.github/linters/hooks/post-merge;
 ln -s ../../../../../.github/linters/hooks/local-post-merge.sh .git/modules/.github/linters/hooks/post-merge;
 chmod 777 .git/modules/.github/linters/hooks/post-merge;
+
+rm .git/hooks/post-merge;
+ln -s ../../.github/linters/hooks/parent-post-merge.sh .git/hooks/post-merge;
+chmod 777 .git/hooks/post-merge;
 
 # Restore working directory
 cd $original_pwd;
