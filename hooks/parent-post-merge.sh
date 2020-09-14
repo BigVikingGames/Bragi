a99c0ddd68f0fd66e8dcc6b23960f4e6a71152f7
nickgkg <nickgkg@gmail.com>
2020-09-14 12:40:19 -0400

    checkout and merge are the same hooks

--- hooks/parent-post-merge.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-if git diff --name-only --ignore-submodules=dirty | grep "\.github/linters"; then
-    git submodule update .github/linters
-    .github/linters/setup/update-all.sh
-fi;
--- setup/github-lint.sh
+++ setup/github-lint.sh
@@ -10,24 +10,24 @@ cd ../../..;
 # Ensure the github action workflow file is up to date
 mkdir -p .github/workflows
 cp .github/linters/linter-workflow.yml .github/workflows/linter-workflow.yml
-git add ./.github
+git add .github
 
 # Setup the post merge hooks to automatically update
 rm .git/modules/.github/linters/hooks/post-merge;
-ln -s ../../../../../.github/linters/hooks/local-post-merge.sh .git/modules/.github/linters/hooks/post-merge;
+ln -s ../../../../../.github/linters/hooks/local-post-change.sh .git/modules/.github/linters/hooks/post-merge;
 chmod 777 .git/modules/.github/linters/hooks/post-merge;
 
 rm .git/hooks/post-merge;
-ln -s ../../.github/linters/hooks/parent-post-merge.sh .git/hooks/post-merge;
+ln -s ../../.github/linters/hooks/parent-post-change.sh .git/hooks/post-merge;
 chmod 777 .git/hooks/post-merge;
 
 # Setup the post checkout hooks to automatically update
 rm .git/modules/.github/linters/hooks/post-checkout;
-ln -s ../../../../../.github/linters/hooks/local-post-checkout.sh .git/modules/.github/linters/hooks/post-checkout;
+ln -s ../../../../../.github/linters/hooks/local-post-change.sh .git/modules/.github/linters/hooks/post-checkout;
 chmod 777 .git/modules/.github/linters/hooks/post-checkout;
 
 rm .git/hooks/post-checkout;
-ln -s ../../.github/linters/hooks/parent-post-checkout.sh .git/hooks/post-checkout;
+ln -s ../../.github/linters/hooks/parent-post-change.sh .git/hooks/post-checkout;
 chmod 777 .git/hooks/post-checkout;
 
 # Restore working directory
