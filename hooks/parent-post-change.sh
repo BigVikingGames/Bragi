022cf3af8249162b0e726fc089ebae935a665a45
nickgkg <nickgkg@gmail.com>
2020-09-15 10:12:11 -0400

    don't submodule update on pre-commit

--- hooks/parent-post-change.sh
+++ hooks/parent-post-change.sh
@@ -2,6 +2,8 @@
 
 echo "merge or checkout $0 upstream: $1 current: $2";
 if git diff --name-only --ignore-submodules=dirty | grep "\.github/linters"; then
-    git submodule update .github/linters
+    if [[ "$1" -ne "$2" ]]; then
+        git submodule update .github/linters
+    fi;
     .github/linters/setup/update-all.sh
 fi;
