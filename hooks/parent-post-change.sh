1751d9d1175dad4e3e5797bad66b09a1d44cb520
nickgkg <nickgkg@gmail.com>
2020-09-15 10:06:57 -0400

    fix

--- hooks/parent-post-change.sh
+++ hooks/parent-post-change.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "merge or checkout $0";
+echo "merge or checkout $0 $1";
 if git diff --name-only --ignore-submodules=dirty | grep "\.github/linters"; then
     git submodule update .github/linters
     .github/linters/setup/update-all.sh
