79d4039e2ae22ea2965e0e44960ab6d32b1b50aa
nickgkg <nickgkg@gmail.com>
2020-09-18 14:27:29 -0400

    fix linting [[]]

--- hooks/parent-post-change.sh
+++ hooks/parent-post-change.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if git diff --name-only --ignore-submodules=dirty | grep "\.github/linters"; then
-    if [[ "$1" != "$2" ]]; then
+    if [ "$1" != "$2" ]; then
         git submodule update .github/linters;
     fi;
     bash -c ".github/linters/scripts/setup-all.sh";
