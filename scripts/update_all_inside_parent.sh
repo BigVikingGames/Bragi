8495d4ee7f879e604bc83bb91b99aa7c17dc02ec
nickgkg <nickgkg@gmail.com>
2020-09-25 11:20:14 -0400

    flags + colors

--- scripts/update_all_inside_parent.sh
+++ scripts/update_all_inside_parent.sh
@@ -1,14 +1,36 @@
 #!/bin/sh
-git_dir="$1";
-auto_clone=true;
+git_dir="";
+auto_clone='false';
+
+print_usage() {
+    printf "Usage:  -g {git_dir} to specify the path to your parent git folder.
+        -c to specify that missing repos are to be automatically cloned (default: false)";
+}
+
+while getopts 'cg:' flag; do
+    case "${flag}" in
+        c) auto_clone='true' ;;
+        g) git_dir="${OPTARG}" ;;
+        *) print_usage
+            exit 1 ;;
+    esac
+done
 
 if [ "$git_dir" == "" ]; then
-    echo "Missing argument 1: Path to your parent git folder.";
+    print_usage;
     exit 1;
 fi;
 
-original_pwd=$(pwd);
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+YWLLOW='\033[1;33m'
+NC='\033[0m' # No Color
+
 update_script="../Bragi/scripts/update_inside_parent.sh";
+
+cd "$git_dir" || exit 1;
 
 update () {
     repo_branch="$1";
@@ -16,34 +38,39 @@ update () {
     repos="$3";
     for repo in $repos; do
         (
-            if $auto_clone; then
-                if ! cd "$git_dir/$repo" > /dev/null 2> /dev/null; then
-                    cd "$git_dir" || exit 1;
-                    git clone "git@github.com:BigVikingGames/$repo.git";
+            echo "Updating branch PTR-linter-$repo_branch for $repo.";
+            if [ ! -d "$repo" ]; then
+                if "$auto_clone"; then
+                    echo "- Status: Cloning missing repo.";
+                    git clone "git@github.com:BigVikingGames/$repo.git" > /dev/null;
+                else
+                    echo "- Status:$YELLOW Missing repo.  Use -c to automatically clone it.$NC";
                 fi;
             fi;
 
-            if cd "$git_dir/$repo" > /dev/null 2> /dev/null; then
-                echo "Updating branch for $repo.";
+            if cd "$repo" > /dev/null 2> /dev/null; then
                 $update_script "$repo_branch" "$bragi_branch" > /dev/null 2> /dev/null;
                 changed=$(git diff "origin/$repo_branch" "origin/PTR-linter-$repo_branch" --name-only 2> /dev/null);
                 unexpected_changed=$(echo "$changed" | grep -vwE "(\.github/linters|\.github/workflows/linter-workflow.yml|\.gitmodules)");
                 if [ "$unexpected_changes" == "" ]; then
                     if [ "$changed" != "" ]; then
-                        echo "- Status: Needs PR";
+                        echo "- Status:$ORANGE Needs PR creates and/or merged.$NC";
+                    else
+                        echo "- Status:$GREEN Already up to date.$NC";
                     fi;
                 else
-                    echo "- Status: Accidentally committed incorrect files.  Need manual fix";
+                    echo "- Status:$RED Accidentally committed incorrect files.  Need manual fix!$NC";
                 fi;
-            else
-                echo "Skipping $repo since it could not be found inside $git_dir."
+                cd ..;
             fi;
         )
     done;
 }
 
-update master master "
-    gjoll-elli gjoll-relay-server gjoll-app-stream Tarnhelm swftool bvg-unity-commons norns-ptr environment-config-ptr
-    Bragi Nidavellir tools-analytics PTR-Tools-Server BAM BAM-Unity WorldsElectron VirtualVikoins
-";
+#update parent_base_branch bragi_branch
+update master master "BAM
+BAM-Unity";
+#    gjoll-elli gjoll-relay-server gjoll-app-stream Tarnhelm swftool bvg-unity-commons norns-ptr environment-config-ptr
+#    Bragi Nidavellir tools-analytics PTR-Tools-Server BAM BAM-Unity WorldsElectron VirtualVikoins
+#";
 #YoWorld Fish-World Fish-World-Tools Kella Yggdrasil
